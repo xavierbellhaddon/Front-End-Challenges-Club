06ceed953ec606038eef62561c6f574dc5c90dce
Austin Bell <snarkeet@gmail.com>
2020-11-09 21:13:17 -0500

    error message

--- email-signup-form/index.js
+++ email-signup-form/index.js
@@ -1,6 +1,19 @@
 const submit = document.querySelector(".submit")
+const input = document.querySelector(".email")
+
+function validateEmail() 
+{
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(input.value))
+  {
+    console.log("yay!")
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
+
 
 submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("click")
+    validateEmail()
 })
