41129263b93448fc965546408f5340b426f22f12
Austin Bell <snarkeet@gmail.com>
2021-06-14 20:16:23 -0400

    progress animation

--- progress-button/index.js
+++ progress-button/index.js
@@ -1,3 +1,25 @@
+const progress = document.getElementById("progress");
+const buttonText = document.querySelector(".button-text");
+let i = 0;
+let width = 0;
+
+const increment = () => {
+    if (i === 0) {
+        const id = setInterval(frame, 20);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+                width = 0;
+                console.log(buttonText.innerHTML)
+            } else {
+                width++;
+            }
+            progress.style.width = width + "%";
+        }
+    }
+}
+
 document.querySelector("button").addEventListener("click", () => {
-    console.log("clicked");
+    increment();
 });
